18c727c36432775df111c3b7f487a81dd0a16c32
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-26 02:20:45 -0600

    add two AJAX calls at once--search-call & id-call--but localStorage reset issues & need way to get mutiple movies at once.

--- js/app.js
+++ js/app.js
@@ -1,60 +1,140 @@
 (function() {
-  'use strict';
+        'use strict';
 
-  var movies = [];
+        const $search = $('#search');
 
-  var renderMovies = function() {
-    $('#listings').empty();
+        var movies = [];
 
-    for (var movie of movies) {
-      var $col = $('<div class="col s6">');
-      var $card = $('<div class="card hoverable">');
-      var $content = $('<div class="card-content center">');
-      var $title = $('<h6 class="card-title truncate">');
+        var renderMovies = function() {
+            $('#listings').empty();
 
-      $title.attr({
-        'data-position': 'top',
-        'data-tooltip': movie.title
-      });
+            for (var movie of movies) {
+                var $col = $('<div class="col s6">');
+                var $card = $('<div class="card hoverable">');
+                var $content = $('<div class="card-content center">');
+                var $title = $('<h6 class="card-title truncate">');
 
-      $title.tooltip({ delay: 50, });
-      $title.text(movie.title);
+                $title.attr({
+                    'data-position': 'top',
+                    'data-tooltip': movie.title
+                });
 
-      var $poster = $('<img class="poster">');
+                $title.tooltip({
+                    delay: 50,
+                });
+                $title.text(movie.title);
 
-      $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
-      });
+                var $poster = $('<img class="poster">');
 
-      $content.append($title, $poster);
-      $card.append($content);
+                $poster.attr({
+                    src: movie.poster,
+                    alt: `${movie.poster} Poster`
+                });
 
-      var $action = $('<div class="card-action center">');
-      var $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
+                $content.append($title, $poster);
+                $card.append($content);
 
-      $plot.attr('href', `#${movie.id}`);
-      $plot.text('Plot Synopsis');
+                var $action = $('<div class="card-action center">');
+                var $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
 
-      $action.append($plot);
-      $card.append($action);
+                $plot.attr('href', `#${movie.id}`);
+                $plot.text('Plot Synopsis');
 
-      var $modal = $(`<div id="${movie.id}" class="modal">`);
-      var $modalContent = $('<div class="modal-content">');
-      var $modalHeader = $('<h4>').text(movie.title);
-      var $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      var $modalText = $('<p>').text(movie.plot);
+                $action.append($plot);
+                $card.append($action);
 
-      $modalContent.append($modalHeader, $movieYear, $modalText);
-      $modal.append($modalContent);
+                var $modal = $(`<div id="${movie.id}" class="modal">`);
+                var $modalContent = $('<div class="modal-content">');
+                var $modalHeader = $('<h4>').text(movie.title);
+                var $movieYear = $('<h6>').text(`Released in ${movie.year}`);
+                var $modalText = $('<p>').text(movie.plot);
 
-      $col.append($card, $modal);
+                $modalContent.append($modalHeader, $movieYear, $modalText);
+                $modal.append($modalContent);
 
-      $('#listings').append($col);
+                $col.append($card, $modal);
 
-      $('.modal-trigger').leanModal();
-    }
-  };
+                $('#listings').append($col);
 
-  // ADD YOUR CODE HERE
-})();
+                $('.modal-trigger').leanModal();
+            }
+        };
+
+        $(function() {
+
+                function getPlot() {
+                    console.log('getting plot');
+                    const fields = {
+                        'Plot': 'plot'
+                    };
+                    //load id info from localStorage OR API
+                    console.log('plot retrieval underway');
+                    var ajaxObject = JSON.parse(localStorage.getItem('ajax'));
+                    var id = ajaxObject.imdbID;
+                    var data = $.getJSON('http://www.omdbapi.com/?', ('i=' + id + '&plot=full&r=json'));
+                    data.done(function(data) {
+                            localStorage.setItem('ajaxID', JSON.stringify(data));
+                            // $("a:contains('Plot Synopsis')").off('click');
+                            Object.assign(movies[0], (translateJXHR(data, fields))); renderMovies();
+                                return;
+                            }); data.fail(function() {
+                            return 'error getting ID for plot';
+                        }); console.log('exiting getPlot');
+                        return;
+                    }
+
+                    function translateJXHR(obj, searchVals) {
+                        console.log('translating', obj);
+                        var movie = {};
+                        for (let i in searchVals) {
+                            movie[searchVals[i]] = obj[i];
+                        }
+
+                        return movie;
+                    }
+
+                    function getMovies() {
+                        const fields = {
+                            'Poster': 'poster',
+                            'Title': 'title',
+                            'Year': 'year',
+                            'imdbID': 'id'
+                        };
+                        //load info from API OR localStorage
+                        var data = $.getJSON('http://www.omdbapi.com/?', ('t=' + $search.val()));
+
+                        data.done(function(data) {
+                          console.log(data);
+                            localStorage.setItem('ajaxMovie', JSON.stringify(data));
+                            movies = [translateJXHR(data, fields)];
+                            //add click to card
+                            // $("a:contains('Plot Synopsis')").click(getPlot);
+                            renderMovies();
+                            return;
+                        });
+
+                        data.fail(function() {
+                            return console.log('sorry:', $search.val(), 'unusable');
+                        });
+                        return;
+                    }
+
+                    function searchSubmit(event) {
+                        event.preventDefault();
+                        console.log('you\'re searching', $search.val(), '!');
+                        if ($search.val()) {
+                            // localStorage.clear();
+                            getMovies();
+                            getPlot();
+                        } else {
+                            console.log('failure to search');
+                        }
+                        return;
+                    }
+
+                    $('form').submit(searchSubmit);
+
+                    // $search.change(function() {});
+                });
+
+        })();
